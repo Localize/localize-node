8ac94898d64b717deb868de6b5a2bbdd60a95c56
kantharubanpayoda <kantharuban.s@payoda.com>
2019-03-29 18:38:27 +0530

    API's as node module -first commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # localize-services
 Wrapper for Localize public API's
+
+Installation
+------------
+
+Install via [npm](http://npmjs.org/)
+
+    npm install localize-services --save
+
+
+Initialize with your localize keys. 
+
+    var localizeServices = require('localize-services')(key);
+
+
+Endpoints
+----------
+  
+- All callbacks are passed an error and response: `callback(err, res)`.
+- Supports camelCase and underscore naming conventions (Gengo uses the underscore convention).
+- Please refer to Localize's [API Docs](https://help.localizejs.com/reference#api-overview) for endpoint details.
+
---
+++ main.js
@@ -0,0 +1,325 @@
+var _               = require('underscore'),
+    request         = require('request');
+
+//GLobals
+var apibase = 'https://api.localizejs.com/v2.0/';
+
+
+// GET request to mstranslator with an subscriptionKey
+var getRequestWithApi = function() {
+    return function(path, headerData, done) {
+        let options = {
+            method: 'GET',
+            url: apibase + path,
+            headers: headerData,
+            json: true,
+        };
+        request.get(options, globalResponseHandler(options, done));
+    };
+};
+
+var postRequestWithApi = function(){
+  return function(path, headerData, data, done) {
+    let options = {
+        method: 'POST',
+        url: apibase + path,
+        headers: headerData,
+        body:data,
+        json: true,
+    };
+    request.post(options, globalResponseHandler(options, done));
+  };
+};
+
+var putRequestWithApi = function(){
+  return function(path, headerData, data, done) {
+    let options = {
+        method: 'PUT',
+        url: apibase + path,
+        headers: headerData,
+        body:data,
+        json: true,
+    };
+    request.put(options, globalResponseHandler(options, done));
+  };
+};
+
+var deleteRequestWithApi = function(){
+  return function(path, headerData, done) {
+    console.log("delete");
+    let options = {
+        method: 'DELETE',
+        url: apibase + path,
+        headers: headerData,
+        json: true,
+    };
+    request.delete(options, globalResponseHandler(options, done));
+  };
+};
+
+var globalResponseHandler = function(request, done) {
+    return function(err, res, body) {
+      var msg;
+      console.log(JSON.stringify(body));
+      if (!done || !_.isFunction(done)) return;
+  
+      // Catch connection errors
+      if (err || !res || res.statusCode !== 200) return done({
+        error: err,
+        response: res,
+        body: body,
+        request: request,
+        toString: function() {
+          return err ? err.toString() : '';
+        },
+      }, null);
+  
+      // Return response
+      done(null, body);
+    };
+  };
+
+////
+//   PUBLIC API
+////
+
+module.exports = function(apiKey) {
+    var get = getRequestWithApi(),
+        post = postRequestWithApi(),
+        put = putRequestWithApi(),
+        deleteCall = deleteRequestWithApi(),
+        api = {};
+        headerType1 = {
+          'Authorization': 'Bearer '+apiKey,
+          'Content-type': 'application/json',
+        },
+        headerType2 = {
+          'Authorization': 'Bearer '+apiKey,
+        }
+
+    ///
+    //   Get Projects
+    ////
+
+    api.getProjects = function(done){
+        get('', headerType1, function(err, result){
+            if (err) return done(err);
+            done(null, result);
+        })
+    }
+
+    ///
+    //   Create Project
+    ////
+
+    api.createProject = function(data, done){
+      const endPoint = 'projects/';
+      post(endPoint, headerType1, data, function(err, result){
+          if (err) return done(err);
+          done(null, result);
+      })
+    }
+    
+    ///
+    //   Get a project
+    ////
+
+    api.getAProject = function(data, done){
+      const endPoint = 'projects/' + data.projectKey + '/';
+      get(endPoint, headerType2, function(err, result){
+          if (err) return done(err);
+          done(null, result);
+      })
+    }
+
+    ///
+    //   Create a phrase
+    ////
+
+    api.createAPhrase = function(data, done){
+      const endPoint = 'projects/' + data.projectKey + '/phrases';
+      post(endPoint, headerType1, data.phraseList ,function(err, result){
+          if (err) return done(err);
+          done(null, result);
+      })
+    }
+
+    ///
+    //   Get Phrases
+    ////
+
+    api.getPhrases = function(data, done){
+      var queryParams = '?';
+      if(data.limit){
+        queryParams = queryParams + "limit=" + data.limit + "&";
+      }
+      if(data.skip){
+        queryParams = queryParams + "skip=" + data.skip + "&";
+      }
+      if(data.state){
+        queryParams = queryParams + "state=" + data.state + "&";
+      }
+      if(data.labels){
+        queryParams = queryParams + "labels=" + data.labels + "&";
+      }
+      if(data.context){
+        queryParams = queryParams + "context=" + data.context + "&";
+      }
+      const endPoint = 'projects/' + data.projectKey + '/phrases' + queryParams;
+      get(endPoint, headerType1, function(err, result){
+          if (err) return done(err);
+          done(null, result);
+      })
+    }
+
+    ///
+    //   Get a phrase
+    ////
+
+    api.getAPhrase = function(data, done){
+      const endPoint = 'projects/' + data.projectKey + '/phrases/' + data.id;
+      get(endPoint, headerType1, function(err, result){
+          if (err) return done(err);
+          done(null, result);
+      })
+    }
+
+    ///
+    //   Delete a phrase
+    ////
+
+    api.deleteAPhrase = function(data, done){
+      const endPoint = 'projects/'+ data.projectKey + '/phrases/' + data.id;
+      deleteCall(endPoint, headerType2, function(err, result){
+          if (err) return done(err);
+          done(null, result);
+      })
+    }
+
+    ///
+    //   Create a phrase
+    ////
+
+    api.createATranslation = function(data, done){
+      const endPoint = 'projects/' + data.projectKey + '/translations/';
+      console.log(JSON.stringify(data.translationDetails));
+      post(endPoint, headerType1, data.translationDetails ,function(err, result){
+          if (err) return done(err);
+          done(null, result);
+      })
+    }
+
+    ///
+    //   Get translations
+    ////
+
+    api.getTranslations = function(data, done){
+      const endPoint = 'projects/' + data.projectKey + '/translations?language=' + data.language ;
+      get(endPoint, headerType2, function(err, result){
+          if (err) return done(err);
+          done(null, result);
+      })
+    }
+
+    ///
+    //   Get a translations
+    ////
+
+    api.getATranslation = function(data, done){
+      const endPoint = 'projects/' + data.projectKey + '/translations/' + data.translationId + '?language=' + data.language ;
+      console.log(endPoint);
+      get(endPoint, headerType2, function(err, result){
+          if (err) return done(err);
+          done(null, result);
+      })
+    }
+
+    ///
+    //   Update a translation
+    ////
+
+    api.updateATranslation = function(data, done){
+      const endPoint = 'projects/' + data.projectKey + '/translations/' + data.translationId;
+      put(endPoint, headerType1, data.translationDetails ,function(err, result){
+          if (err) return done(err);
+          done(null, result);
+      })
+    }
+
+    ///
+    //   Delete a translation
+    ////
+
+    api.deleteATranslation = function(data, done){
+      const endPoint = 'projects/' + data.projectKey + '/translation/' + data.translationId +'/';
+      deleteCall(endPoint, headerType2, function(err, result){
+          if (err) return done(err);
+          done(null, result);
+      })
+    }
+
+    ///
+    //   Get languages
+    ////
+
+    api.getLanguages = function(data, done){
+      var code = '';
+      if(data.code){
+        code = data.code;
+      }
+      const endPoint = 'languages?code=' + code;
+      get(endPoint, headerType2, function(err, result){
+          if (err) return done(err);
+          done(null, result);
+      })
+    }
+
+    ///
+    //   Get team members
+    ////
+
+    api.getTeamMembers = function(data, done){
+      const endPoint = 'projects/' + data.projectKey + '/team-members';
+      get(endPoint, headerType2, function(err, result){
+          if (err) return done(err);
+          done(null, result);
+      })
+    }
+
+    ///
+    //   Get team members
+    ////
+
+    api.translatePhrase = function(data, done){
+      var sourceLanguage = '';
+      if(data.sourceLanguage){
+        sourceLanguage = data.sourceLanguage;
+      }
+      const endPoint = 'projects/' + data.projectKey + '/machine-translations/translate?phrase=' + data.phrase + '&targetLanguage=' + data.targetLanguage + '&sourceLanguage=' + sourceLanguage;
+      get(endPoint, headerType2, function(err, result){
+          if (err) return done(err);
+          done(null, result);
+      })
+    }
+    ///
+    //   RETURN APIextend
+    ////
+  
+    return {
+        testAPI:              api.testAPI,
+        getProjects:          api.getProjects,
+        createProject:        api.createProject,
+        getAProject:          api.getAProject,
+        createAPhrase:        api.createAPhrase,
+        getPhrases:           api.getPhrases,
+        getAPhrase:           api.getAPhrase,
+        deleteAPhrase:        api.deleteAPhrase,
+        createATranslation:   api.createATranslation,
+        getTranslations:      api.getTranslations,
+        getATranslation:      api.getATranslation,
+        updateATranslation:   api.updateATranslation,
+        deleteATranslation:   api.deleteATranslation,
+        getLanguages:         api.getLanguages,
+        getTeamMembers:       api.getTeamMembers
+      };
+}
