15764c9c8ae326eded1abc935b1ce16608bbf237
kantharubanpayoda <kantharuban.s@payoda.com>
2019-04-04 19:21:50 +0530

    test cases for the module

--- test/test.js
+++ test/test.js
@@ -5,79 +5,91 @@ var it = mocha.it
 let chai = require('chai');
 let should = require('should');
 const key = 'b1f14c23539766384d760733f94ba20e';
-const localizeservice = require('../main')(key);
+const Readable = require('stream').Readable;
+const localizeService = require('../main')(key);
 
 describe('Localize APIs', () => {
+    let projectTestData = {
+        projectId: '',
+        phraseId: '',
+        translationId: '',
+    }
 
     it('getlanguagesList', function (done) {
+        this.timeout(20000);
         const data = {
             projectKey:'HnaArqQystM8D'
         };
         
-        localizeservice.languagesList(data, function(err, result){
+        localizeService.project.languages(data, function(err, result){
             if(err) {
-                console.log('error in languagesList');
-                done();
+                console.log('error in languagesList :' + err);
             }
             result.data.languages.should.be.Array();
+            done();
         });
-    }).timeout(10000);;
+    });;
 
     it('translatePhrase', function (done) {
+        this.timeout(10000);
         const data = {
             projectKey:'HnaArqQystM8D',
             phrase:'This is a sample text',
-            targetLanguage:'fr',
-            sourceLanguage:'en',
+            target:'fr',
+            source:'en',
         }
         
-        localizeservice.translatePhrase(data, function(err, result){
+        localizeService.machine.translate(data, function(err, result){
             if(err) {
-                console.log('error in translatePhrase');
-                done();
+                console.log('error in translatePhrase:' + err);
             }
             result.data.translation.should.eql('Ceci est un exemple de texte');
             done();
         });
-    }).timeout(5000);
+    });
 
     it('detectLanguage', function (done) {
+        this.timeout(10000);
         const data = {
             projectKey:'HnaArqQystM8D',
-            phrase:['Ceci est un exemple de texte'],
+            phrase:'Ceci est un exemple de texte',
         }
 
-        localizeservice.detectLanguage(data, function(err, result){
+        localizeService.machine.detectLanguage(data, function(err, result){
             if(err) {
-                console.log('error in detectLanguage');
-                done();
+                console.log('error in detectLanguage:' + err);
             }
-            console.log('call :' + JSON.stringify(result));
             result.data.language.should.eql("fr");
+            done();
         });
-    }).timeout(10000);
+    });
 
-    it('importContent', function (done) {
+    it.skip('importContent', function (done) {
+        this.timeout(10000);
+
+        const stream = new Readable();
+        stream.push(__dirname + '/phrase.xliff');
+        stream.push(null);
         const data = {
             projectKey:'HnaArqQystM8D',
             language:'en',
             format:'XLIFF',
             type:'glossary',
-            content:"application/x-xliff;name=glossary-import-test-file.xliff;base64,PD94bWwgdmVyc2lvbiA9ICIxLjAiIGVuY29kaW5nPSJVVEYtOCI/Pgo8eGxpZmYgdmVyc2lvbj0iMS4yIj4KPGZpbGUgb3JpZ2luYWw9Im1haW4iIHNvdXJjZS1sYW5ndWFnZT0iZGUiIHRhcmdldC1sYW5ndWFnZT0iZW4iPgo8aGVhZD48L2hlYWQ+Cjxib2R5PgogIDx0cmFucy11bml0PgogICAgPHNvdXJjZSB4bWw6bGFuZz0iZGUiPldlbm4gU2llIGRpZXNlIFphaGx1bmdzd2Vpc2UgbnVyIGF1ZiBiZXN0aW1tdGUgTMOkbmRlciBiZXNjaHLDpG5rZW4gd29sbGVuLCBrw7ZubmVuIFNpZSBkaWVzZSBoaWVyIGF1c3fDpGhsZW4uJmx0Oy9zb3VyYyZndDs8L3NvdXJjZT4KICAgIDx0YXJ…dD4KICAgIDxzb3VyY2UgeG1sOmxhbmc9ImRlIj5EaWVzZSAlYW56YWhsJSBTcGVuZGVucXVpdHR1bmdlbiB3ZXJkZW4gaW4gZWluZW0gUERGIHp1c2FtbWVuZ2VmYXNzZW4gdW5kIElobmVuIGFscyBEb3dubG9hZCBhbmdlYm90ZW4uIERlciBWb3JnYW5nIGthbm4gZXR3YXMgWmVpdCBiZWFuc3BydWNoZW4uPC9zb3VyY2U+CiAgICA8dGFyZ2V0IHhtbDpsYW5nPSJlbiI+VGhlc2UgJWFuemFobCUgZG9uYXRpb24gcmVjZWlwdHMgd2lsbCBiZSBtZXJnZWQsIHB1dCBpbnRvIGEgUERGIGFuZCBvZmZlcmVkIGFzIGEgZG93bmxvYWQuIFRoZSBwcm9jZXNzIG1heSB0YWtlIHNvbWUgdGltZS48L3RhcmdldD4KICA8L3RyYW5zLXVuaXQ+CjwvYm9keT4KPC9maWxlPgo8L3hsaWZmPgo=",
+            file:__dirname + '/phrase.xliff',
+            content:stream,
         }
 
-        localizeservice.importContent(data, function(err, result){
+        localizeService.content.import(data, function(err, result){
             if(err) {
-                console.log('error in importContent');
-                done();
+                console.log('error in importContent:' + err);
             }
-            console.log('call importContent:' + JSON.stringify(result));
             done();
-            //result.data.language.should.eql("fr");
+            
         });
     });
 
     it('exportContent', function (done) {
+        this.timeout(10000);
         data = {
             projectKey:'HnaArqQystM8D',
             language: 'en',
@@ -85,124 +97,119 @@ describe('Localize APIs', () => {
             context: 'An active phrase context',
         }
 
-        localizeservice.exportContent(data, function(err, result){
+        localizeService.content.export(data, function(err, result){
             if(err) {
-                console.log('error in exportContent');
-                done();
+                console.log('error in exportContent:' + err);
             }
-            console.log('call exportContent :' + JSON.stringify(result));
+            result.should.have.property('dictionary')
             done();
-            //result.data.language.should.eql("fr");
         });
     });
 
     it('createProject', function (done) {
+        this.timeout(10000);
         data = {
             projectKey:'HnaArqQystM8D',
             name: 'testing1',
             sourceLanguage: 'en',
         }
 
-        localizeservice.createProject(data, function(err, result){
+        localizeService.project.create(data, function(err, result){
             if(err) {
-                console.log('error in createProject');
-                done();
+                console.log('error in createProject:' + err);
             }
-            console.log('call createProject :' + JSON.stringify(result));
+            projectTestData.projectId = result.data.project.id;
+            result.data.project.should.have.property('id');
             done();
-            //result.data.language.should.eql("fr");
         });
     });
 
     it('getAProject', function (done) {
+        this.timeout(10000);
         data = {
             projectKey:'HnaArqQystM8D',
         }
 
-        localizeservice.getAProject(data, function(err, result){
+        localizeService.project.getOne(data, function(err, result){
             if(err) {
-                console.log('error in getAProject');
-                done();
+                console.log('error in getAProject:' + err);
             }
-            console.log('call getAProject :' + JSON.stringify(result));
             done();
-            //result.data.language.should.eql("fr");
+            result.data.project.should.have.property('id');
         });
     });
 
 
     it('createAPhrase', function (done) {
+        this.timeout(10000);
         data = {
             projectKey:'HnaArqQystM8D',
             phraseList:{ "phrases": [ { "phrase": "My new phrase", "context":"this phrase has context"} ] }
         }
 
-        localizeservice.createAPhrase(data, function(err, result){
+        localizeService.phrase.create(data, function(err, result){
             if(err) {
-                console.log('error in createAPhrase');
-                done();
+                console.log('error in createAPhrase:' + err);
             }
-            console.log('call createAPhrase :' + JSON.stringify(result));
+            result.meta.status.should.eql(200);
             done();
-            //result.data.language.should.eql("fr");
         });
-    });
+    }).timeout(5000);
 
     it('getPhrases', function (done) {
+        this.timeout(10000);
         data = { 
             "projectKey" : "HnaArqQystM8D", 
-            "limit":"2", 
-            "skip":"1", 
-            "labels":"['sad']", 
-            "context":"" 
+            // "limit":"2", 
+            // "skip":"1", 
+            // "labels":"", 
+            "context":"this phrase has context", 
         }
 
-        localizeservice.getPhrases(data, function(err, result){
+        localizeService.phrase.getAll(data, function(err, result){
             if(err) {
-                console.log('error in getPhrases');
-                done();
+                console.log('error in getPhrases:' + err);
             }
-            console.log('call getPhrases :' + JSON.stringify(result));
+            projectTestData.phraseId = result.data.phrases[0].id;
+            result.data.phrases.should.be.an.Array();
             done();
-            //result.data.language.should.eql("fr");
         });
     });
 
     it('getAPhrase', function (done) {
-        data =  { 
+        this.timeout(10000);
+        data =  {
             "projectKey" : "HnaArqQystM8D",
-             "id":"5c9c70b24696107ae33830a7" 
+             "id": projectTestData.phraseId, 
             }
 
-        localizeservice.getAPhrase(data, function(err, result){
+        localizeService.phrase.getOne(data, function(err, result){
             if(err) {
-                console.log('error in getAPhrase');
-                done();
+                console.log('error in getAPhrase:' + err);
             }
-            console.log('call getAPhrase :' + JSON.stringify(result));
+            result.data.phrases.should.be.an.Array();
             done();
-            //result.data.language.should.eql("fr");
         });
     });
 
     it('deleteAPhrase', function (done) {
+        this.timeout(10000);
         data =  { 
             "projectKey" : "HnaArqQystM8D",
-             "id":"5c9c70b24696107ae33830a7" 
+             "id":projectTestData.phraseId, 
             }
 
-        localizeservice.deleteAPhrase(data, function(err, result){
+        localizeService.phrase.deleteOne(data, function(err, result){
             if(err) {
-                console.log('error in deleteAPhrase');
-                done();
+                console.log('error in deleteAPhrase:' + err);
             }
-            console.log('call deleteAPhrase :' + JSON.stringify(result));
+            result.meta.status.should.eql(200);
             done();
-            //result.data.language.should.eql("fr");
         });
     });
 
     it('createATranslation', function (done) {
+        this.timeout(10000);
         data =  { 
             "translationDetails": { 
                 "phrase":"5c9c70b24696107ae33830a8",
@@ -214,56 +221,55 @@ describe('Localize APIs', () => {
              "projectKey":"HnaArqQystM8D" 
             }
 
-        localizeservice.createATranslation(data, function(err, result){
+        localizeService.translation.create(data, function(err, result){
             if(err) {
-                console.log('error in createATranslation');
-                done();
+                console.log('error in createATranslation:' + err);
             }
-            console.log('call createATranslation :' + JSON.stringify(result));
+            projectTestData.translationId = result.data.translation.id;
+            result.data.translation.should.have.property('id');
             done();
-            //result.data.language.should.eql("fr");
         });
     });
 
     it('getTranslations', function (done) {
+        this.timeout(10000);
         data =  { 
             "language":"fr",
              "projectKey":"HnaArqQystM8D" 
             }
 
-        localizeservice.getTranslations(data, function(err, result){
+        localizeService.translation.getAll(data, function(err, result){
             if(err) {
-                console.log('error in getTranslations');
-                done();
+                console.log('error in getTranslations:' + err);
             }
-            console.log('call getTranslations :' + JSON.stringify(result));
+            result.data.translations.should.be.an.Array();
             done();
-            //result.data.language.should.eql("fr");
         });
     });
 
     it('getATranslation', function (done) {
+        this.timeout(10000);
         data =  { 
             "projectKey" : "HnaArqQystM8D",
              "language":"en" ,
-             "translationId":"5c9c711145963b0fb655575f"
+             "translationId": projectTestData.translationId,
             }
 
-        localizeservice.getATranslation(data, function(err, result){
+        localizeService.translation.getOne(data, function(err, result){
             if(err) {
-                console.log('error in getATranslation');
-                done();
+                console.log('error in getATranslation:' + err);
             }
-            console.log('call getATranslation :' + JSON.stringify(result));
+            result.meta.status.should.be.eql(200);
+            result.data.translation.should.have.property('id');
             done();
-            //result.data.language.should.eql("fr");
         });
     });
 
     it('updateATranslation', function (done) {
+        this.timeout(10000);
         data = {
             "projectKey" : "HnaArqQystM8D",
-             "translationId":"5c9c711145963b0fb655575f",
+             "translationId":projectTestData.translationId,
               "translationDetails" : { 
                   "value":"nieuwe zin",
                    "state":"active",
@@ -271,62 +277,58 @@ describe('Localize APIs', () => {
                 }
             }
 
-        localizeservice.updateATranslation(data, function(err, result){
+        localizeService.translation.update(data, function(err, result){
             if(err) {
-                console.log('error in updateATranslation');
-                done();
+                console.log('error in updateATranslation:' + err);
             }
-            console.log('call updateATranslation :' + JSON.stringify(result));
+            result.meta.status.should.be.eql(200);
+            result.data.translation.should.have.property('id');
             done();
-            //result.data.language.should.eql("fr");
         });
     });
 
     it('deleteATranslation', function (done) {
+        this.timeout(10000);
         data = {
             "projectKey" : "HnaArqQystM8D",
-            "translationId":"5c9c711145963b0fb655575f",
+            "translationId":projectTestData.translationId,
         }
 
-        localizeservice.deleteATranslation(data, function(err, result){
+        localizeService.translation.deleteOne(data, function(err, result){
             if(err) {
-                console.log('error in deleteATranslation');
-                done();
+                console.log('error in deleteATranslation:' + err);
             }
-            console.log('call deleteATranslation :' + JSON.stringify(result));
+            result.meta.status.should.be.eql(200);
             done();
-            //result.data.language.should.eql("fr");
         });
     });
 
 
     it('getLanguages', function (done) {
+        this.timeout(10000);
         data = {
             "projectKey" : "HnaArqQystM8D",
             "code" : "en",
         }
 
-        localizeservice.getLanguages(data, function(err, result){
+        localizeService.project.languages(data, function(err, result){
             if(err) {
-                console.log('error in getLanguages');
-                done();
+                console.log('error in getLanguages:' + err);
             }
-            console.log('call getLanguages :' + JSON.stringify(result));
+            result.data.languages.should.be.an.Array();
             done();
-            //result.data.language.should.eql("fr");
         });
     });
 
     it('getTeamMembers', function (done) {
+        this.timeout(10000);
         const data = {
             "projectKey" : "HnaArqQystM8D",
         }
-        localizeservice.getTeamMembers(data,function(err,result){
+        localizeService.project.getTeam(data,function(err,result){
             if(err) {
-                console.log('error in getTeamMembers');
-                done();
+                console.log('error in getTeamMembers:' + err);
             }
-            //console.log('call :' + JSON.stringify(result));
             result.data.team.should.be.an.Array();
             done();
         });
